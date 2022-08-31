95f09faf450f20bb94f99ce5185ff290334a1e19
Pedro Goncalves <p.goncalves2002@gmail.com>
2022-08-31 11:41:18 -0300

    .

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 
+struct agenda{
+    char nome[50];
+    char telefone[50];
+    struct data dataDeNascimento;
+};
+struct data{
+    int dia;
+    char mes[20];
+    int ano;
+};
 int main() {
-    printf("Hello, World!\n");
+    int N_agendas;
+    printf("digite o numero de agnedas que desaj criar:\n");
+    scanf("%d", &N_agendas);
+
+    struct agenda agendas[N_agendas];
+    for (int i = 0; i < N_agendas; ++i) {
+        printf("digite seu nome");
+        scanf("%s", &agendas[i].nome);
+        printf("digite seu telefone");
+        scanf("%s", &agendas[i].telefone);
+        printf("digite o dia do seu nascimento");
+        scanf("%d", &agendas[i].dataDeNascimento.dia);
+        printf("digite o mes do seu nascimento");
+        scanf("%s", &agendas[i].dataDeNascimento.mes);
+        printf("digite o ano do seu nascimento");
+        scanf("%d", &agendas->dataDeNascimento.ano);
+
+    }
+    for (int i = 0; i < N_agendas; ++i) {
+        printf("%s", agendas[i].nome);
+        printf("%s", agendas[i]telefone);
+
+    }
     return 0;
 }
